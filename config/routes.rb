72aa2a90396334ac771572f6951734406783de3a
Lepkele <charlessport2000@yahoo.com>
2016-12-18 08:18:22 +0000

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   get '/home', to: 'pages#home'
+
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
   
- 
-  
-  resources :recipes
 
 end
